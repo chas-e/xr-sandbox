849d29ec8bdb73052c1931862e7f08f60cd3d8da
chas-e <sigmapi76388@gmail.com>
2020-11-18 16:45:07 -0500

    updating and adding files

---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function Home(props) {
+    return (
+        <>
+            <main>
+                <h1>Welcome to AngellXR's WebXR Sandbox</h1>
+            </main>
+        </>
+    );
+}
